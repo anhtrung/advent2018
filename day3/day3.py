3a7d4e16e649d93ddd7a9f33f76f3ba5042015d8
anh <anh.t.trung@gmail.com>
2018-12-03 21:36:20 +1100

    day3: initial python solution

---
+++ day3/day3.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+
+
+def parse_claim(claim):
+    id = claim[1:claim.find(' ')]
+    pos = claim[claim.find('@') + 1:claim.find(':')].split(',')
+    dim = claim[claim.find(':') + 1:].split('x')
+    return pos, dim
+
+dir = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(dir, 'input')) as f:
+    input_data = f.read()
+lines = input_data.splitlines()
+
+fabric = np.zeros((2000, 2000), dtype=int)
+
+for l in lines:
+    p, d = parse_claim(l)
+    fabric[int(p[0]):int(p[0]) + int(d[0]), int(p[1]):int(p[1]) + int(d[1])] += 1
+
+print("Part 2: " + str((fabric > 1).sum()))
