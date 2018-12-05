2681b91e51a31027bb929b7b605270b4e498a3ae
anh <anh.t.trung@gmail.com>
2018-12-05 21:07:24 +1100

    day5: refactored + part2

--- day5/day5.py
+++ day5/day5.py
@@ -1,19 +1,31 @@
 import os
+import string
 
+def react(poly):
+    i = 0
+    while True:
+        if i == len(poly) - 1:
+            return len(poly)
+        if poly[i].lower() == poly[i+1].lower() and poly[i].islower() != poly[i+1].islower():
+            poly = poly[:i] + poly[i+2:]
+            i = max(0, i - 1)
+        else:
+            i += 1
+            
 dir = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(dir, 'input')) as f:
     input_data = f.read()
 
 poly = input_data.strip()
 
-last_len = len(poly)
-while True:
-    for i in range(len(poly) - 1):
-        if poly[i].lower() == poly[i+1].lower() and poly[i].islower() != poly[i+1].islower():
-            poly = poly[:i] + poly[i+2:]
-            break
-    if len(poly) == last_len:
-        break
-    last_len = len(poly)
+part1 = react(poly)
+print("Part 1: " + str(part1))
 
-print("Part 1: " + str(len(poly)))
+alpha = string.ascii_lowercase[:]
+
+counts = {}
+for letter in alpha:
+    poly = input_data.strip().replace(letter, '').replace(letter.upper(), '')
+    counts[letter] = react(poly)
+
+print("Part 2: " + str(min(counts.values())))
