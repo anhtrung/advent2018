e142add7424ea1be456f991446de12baed238213
anh <anh.t.trung@gmail.com>
2018-12-08 22:57:39 +1100

    day8: python refactored

--- day8/day8.py
+++ day8/day8.py
@@ -1,42 +1,33 @@
 import os
+from functools import reduce
 
 
+def get_node(tree):
+    num_child_nodes, num_metadata_entries = tree[0:2]
+    data = tree[2:]
 
-def get_node(tree, start_idx):
-    num_child_nodes = tree[start_idx]
-    num_metadata_entries = tree[start_idx + 1]
+    node_values = []
+    node_total_sum = 0
+    for _ in range(num_child_nodes):
+        data, md_sum, node_value = get_node(data)
+        node_values.append(node_value)
+        node_total_sum += md_sum
+    metadata = (data[:num_metadata_entries])
 
-    metadata = []
-    own_metadata = []
-    nodes = []
-
-    idx = start_idx + 2
-    for i in range(num_child_nodes):
-        idx, node_metadata, node_value = get_node(tree, idx)
-        metadata.extend(node_metadata)
-        nodes.append((idx, node_metadata, node_value))
-    for i in range(num_metadata_entries):
-        own_metadata.append(tree[idx + i])
-    metadata.extend(own_metadata)
     if num_child_nodes == 0:
-        value = sum(metadata)
-    else:
-        value = 0
-        for i in own_metadata:
-            if i - 1 < len(nodes):
-                value += nodes[i-1][2]
-    
-    return idx + num_metadata_entries, metadata, value
+        return data[num_metadata_entries:], sum(metadata), sum(metadata)
+        
+    value = sum([node_values[md-1] for md in metadata if md-1 < len(node_values)])
+    return data[num_metadata_entries:], sum(metadata) + node_total_sum, value
+
 
 dir = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(dir, 'input')) as f:
     input_data = f.read()
 
-tree = input_data.strip().split(' ')
-tree = [int(i) for i in tree]
+tree = [int(i) for i in input_data.strip().split(' ')]
 
-_, root_metadata, root_value = get_node(tree, 0)
+_, root_metadata, root_value = get_node(tree)
 
-print("Part 1: " + str(sum(root_metadata)))
-
+print("Part 1: " + str(root_metadata))
 print("Part 2: " + str(root_value))
