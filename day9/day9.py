6147b72bcf03ef9d8e3d341b3f728cca020bceea
anh <anh.t.trung@gmail.com>
2018-12-10 00:34:21 +1100

    day9: python part2 initial solution

--- day9/day9.py
+++ day9/day9.py
@@ -1,29 +1,33 @@
 from collections import defaultdict
 
 num_players = 471
-last_marble_point = 72026
+last_marble_point = 7202600
 
 player_points = defaultdict(int)
-current_marble_pos = 1
-marble_circle = [0, 1]
+current_marble = 0
+
+circle = {0: {"prev": 0, "next": 0}}
+
 current_player = 1
 
-for m in range(2, last_marble_point + 1):
+for m in range(1, last_marble_point + 1):
     current_player = (current_player + 1) % num_players
     if m % 23 == 0:
         player_points[current_player] += m
-        remove_idx = current_marble_pos - 7
-        if remove_idx < 0:
-            remove_idx = len(marble_circle) + remove_idx
-        player_points[current_player] += marble_circle[remove_idx]
-        marble_circle.pop(remove_idx)
-        current_marble_pos = remove_idx
+        for i in range(0, 7):
+            current_marble = circle[current_marble]["prev"]
+        player_points[current_player] += current_marble
+        circle[circle[current_marble]["prev"]]["next"] = circle[current_marble]["next"]
+        circle[circle[current_marble]["next"]]["prev"] = circle[current_marble]["prev"]
+        current_marble = circle[current_marble]["next"]
+
     else:
-        insert_pos = current_marble_pos + 2
-        if insert_pos > len(marble_circle):
-            insert_pos = insert_pos % len(marble_circle)
-        marble_circle.insert(insert_pos, m)
-        current_marble_pos = insert_pos
+        new_marble_prev = circle[current_marble]["next"]
+        new_marble_next = circle[new_marble_prev]["next"]
+        circle[circle[circle[current_marble]["next"]]["next"]]["prev"] = m
+        circle[circle[current_marble]["next"]]["next"] = m        
+        circle[m] = {"prev": new_marble_prev, "next": new_marble_next}
+        current_marble = m
 
 max_score = max(player_points.values())
 
