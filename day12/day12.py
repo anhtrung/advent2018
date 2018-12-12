67f3b3179996fa12ee6a17e05d5755c018d02545
anh <anh.t.trung@gmail.com>
2018-12-12 21:47:22 +1100

    day12: initial solution python part 2

--- day12/day12.py
+++ day12/day12.py
@@ -1,7 +1,4 @@
-import os
-
-dir = os.path.dirname(os.path.realpath(__file__))
-with open(os.path.join(dir, 'input')) as f:
+with open("input.txt", "r") as f:
     s = f.read().splitlines()
 
 initial_state = s[0][15:]
@@ -12,25 +9,56 @@ for rule in s[2:]:
     after = rule[9]
     rules[before] = after
 
-gen_state = '.'*len(initial_state) + initial_state + '.'*len(initial_state)
+gen_state = initial_state
+pot0_idx = 0
 
-for gen in range(20):
-    g = '..'
+for gen in range(500):
+#    if gen % 1000 == 0:
+#        print(gen)
+    spacing = ' '*(20 - pot0_idx)
+#    print(spacing + gen_state)
+    while gen_state.startswith('.........'):
+        gen_state = gen_state[5:]
+        pot0_idx -= 5
+    while gen_state.endswith('.........'):
+        gen_state = gen_state[:-5]
+
+    if not gen_state.startswith('....'):
+        gen_state = '....' + gen_state
+        pot0_idx += 4
+    if not gen_state.endswith('....'):
+        gen_state += '....'
+
+    pot0_idx -= 2
+    g = ''
     for i in range(2, len(gen_state) - 2):
         xx = gen_state[i-2:i+3]
         if xx not in rules:
             g += '.'
         else:
             g += rules[xx]
-    g += '..'
     gen_state = g
-    print(g)
+
+    sum = 0
+    for idx, pot in enumerate(gen_state):
+        if pot == '#':
+            sum += idx - pot0_idx
+
+    print(sum)
+
+
+spacing = ' ' * (20 - pot0_idx)
+print(spacing + gen_state)
 
 c = gen_state.count('#')
+print(c)
 
 sum = 0
 for idx, pot in enumerate(gen_state):
     if pot == '#':
-        sum += idx - len(initial_state)
-
+        sum += idx - pot0_idx
 print(sum)
+
+
+total = (50000000000 - 500)*c + sum
+print(total)
