04521360874286566f78ebfdfecd7f4d978ad248
anh <anh.t.trung@gmail.com>
2018-12-13 20:31:35 +1100

    day13: python cleaned up

--- day13/day13.py
+++ day13/day13.py
@@ -1,97 +1,78 @@
 import os
+
 dir = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(dir, 'input')) as f:
-    s = f.read().splitlines()
+    input_map = f.read().splitlines()
 
-carts = []
 dirs = ["<", "^", ">", "v"]
-dirs_L = ["L", "U", "R", "D"]
 map = []
+carts = []
 
-for row, line in enumerate(s):
+def turn_left(dir):
+    d = dirs.index(dir)
+    return dirs[d - 1]
+
+def turn_right(dir):
+    d = dirs.index(dir)
+    return dirs[(d + 1) % len(dirs)]
+
+for row, line in enumerate(input_map):
     map_line = ""
     for col, ch in enumerate(line):
         if ch in dirs:
-            if ch == '>':
-                dir = "R"
-                re = "-"
-            elif ch == "<":
-                dir = "L"
-                re = "-"
-            elif ch == "^":
-                dir = "U"
+            if ch == '>' or '<':
+                re = '-'
+            elif ch == '^' or 'v':
                 re = "|"
-            elif ch == "v":
-                dir = "D"
-            re = "|"
-            carts.append({"row": row, "col": col, "dir": dir, "xs": 0})
+            carts.append({"row": row, "col": col, "dir": ch, "xs": 0})
             map_line += re
         else:
             map_line += ch
-
     map.append(map_line)
-
-carts = sorted(carts, key=lambda x: (x["row"], x["col"]))
-#print(carts)
 
 for t in range(0, 1000000):
     carts = sorted(carts, key=lambda x: (x["row"], x["col"]))
-    carts_to_remove = []
+    crashed_carts_idx = []
+
     for c in carts:
-        if c["dir"] == "R":
+        if c["dir"] == ">":
             c["col"] += 1
-        elif c["dir"] == "L":
+        elif c["dir"] == "<":
             c["col"] -= 1
-        elif c["dir"] == "U":
+        elif c["dir"] == "^":
             c["row"] -= 1
-        else:
+        elif c["dir"] == "v":
             c["row"] += 1
 
-        if map[c["row"]][c["col"]] == "/" and c["dir"] == "U":
-            c["dir"] = "R"
-        elif map[c["row"]][c["col"]] == "/" and c["dir"] == "D":
-            c["dir"] = "L"
-        elif map[c["row"]][c["col"]] == "/" and c["dir"] == "R":
-            c["dir"] = "U"
-        elif map[c["row"]][c["col"]] == "/" and c["dir"] == "L":
-            c["dir"] = "D"
-        elif map[c["row"]][c["col"]] == "\\" and c["dir"] == "U":
-            c["dir"] = "L"
-        elif map[c["row"]][c["col"]] == "\\" and c["dir"] == "D":
-            c["dir"] = "R"
-        elif map[c["row"]][c["col"]] == "\\" and c["dir"] == "R":
-            c["dir"] = "D"
-        elif map[c["row"]][c["col"]] == "\\" and c["dir"] == "L":
-            c["dir"] = "U"
-
+        if ((map[c["row"]][c["col"]] == "/" and c["dir"] == "^") or 
+        (map[c["row"]][c["col"]] == "/" and c["dir"] == "v") or
+        (map[c["row"]][c["col"]] == "\\" and c["dir"] == ">") or 
+        (map[c["row"]][c["col"]] == "\\" and c["dir"] == "<")):
+            c["dir"] = turn_right(c["dir"])
+        elif ((map[c["row"]][c["col"]] == "/" and c["dir"] == ">") or
+        (map[c["row"]][c["col"]] == "/" and c["dir"] == "<") or  
+        (map[c["row"]][c["col"]] == "\\" and c["dir"] == "^") or
+        (map[c["row"]][c["col"]] == "\\" and c["dir"] == "v")):
+            c["dir"] = turn_left(c["dir"])
         elif map[c["row"]][c["col"]] == "+":
             turns = c["xs"] % 3
-            d = dirs_L.index(c["dir"])
             if turns == 0:  # turn left
-                c["dir"] = dirs_L[d - 1]
+                c["dir"] = turn_left(c["dir"])
             elif turns == 2: # turn right
-                c["dir"] = dirs_L[(d + 1) % len(dirs_L)]
+                c["dir"] = turn_right(c["dir"])
             c["xs"] += 1
 
-    # check for crashes
-#    carts = sorted(carts, key=lambda x: (x["row"], x["col"]))
-#    print(carts)
+        # check for crashes
+        for i in range(1, len(carts)):
+            if carts[i]["row"] == carts[i-1]["row"] and carts[i]["col"] == carts[i-1]["col"]:
+                print(f"Crash at tick {t} at {c['col']},{c['row']}")
+                crashed_carts_idx.append(i)
+                crashed_carts_idx.append(i-1)
 
-        for i, c in enumerate(carts):
-            if i == 0:
-                pass
-            elif carts[i]["row"] == carts[i-1]["row"] and carts[i]["col"] == carts[i-1]["col"]:
-                print(f"CRASH at time {t} at {c['col']},{c['row']}")
-                carts_to_remove.append(i-1)
-                carts_to_remove.append(i)
-#                print(carts)
-    for cr in sorted(set(carts_to_remove), reverse=True):
-        print(carts[cr])
+    # remove crashed carts
+    for cr in sorted(set(crashed_carts_idx), reverse=True):
         carts.pop(cr)
 
     if len(carts) == 1:
-        print("Last car")
-        print(carts[0])
+        print(f"Last car: {carts[0]['col']},{carts[0]['row']}")
         break
-#for m in map:
-#    print(m)
