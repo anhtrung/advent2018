a8d808053db3ae0c246c11729f68df5d8ea882a6
anh <anh.t.trung@gmail.com>
2018-12-15 01:32:19 +1100

    day14: python intial solution part 2

--- day14/day14.py
+++ day14/day14.py
@@ -1,11 +1,12 @@
 elf_1_idx = 0
 elf_2_idx = 1
 
-
 recipes = [3, 7]
 puzzle_input = 440231
+puzzle_input_list = [4, 4, 0, 2, 3, 1]
 
-for i in range(0, puzzle_input + 10):
+
+for i in range(552860567+20):
     new_recipe = recipes[elf_1_idx] + recipes[elf_2_idx]
     if new_recipe >= 10:
         recipes.append(1)
@@ -14,4 +15,14 @@ for i in range(0, puzzle_input + 10):
     elf_1_idx = (elf_1_idx + 1 + recipes[elf_1_idx]) % len(recipes)
     elf_2_idx = (elf_2_idx + 1 + recipes[elf_2_idx]) % len(recipes)
 
-print(recipes[puzzle_input:puzzle_input+10])
+    if recipes[-len(puzzle_input_list)-1:-1] == puzzle_input_list:
+        print("Part 2: " + str(len(recipes) - len(puzzle_input_list) - 1))
+        break
+
+    if recipes[-len(puzzle_input_list):] == puzzle_input_list:
+        print("Part 2: " + str(len(recipes) - len(puzzle_input_list)))
+        break
+
+part1_answer = ''.join(str(x) for x in recipes[puzzle_input:puzzle_input+10])
+
+print("Part 1: " + part1_answer)
