85395081dd0dff2b97133bbff83cbf08eaf34e6c
anh <anh.t.trung@gmail.com>
2018-12-18 19:21:42 +1100

    day18: python refactored, part 2

--- day18/day18.py
+++ day18/day18.py
@@ -1,53 +1,33 @@
-import copy
+import numpy as np
+import scipy.signal
 import os
 
 dir = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(dir, 'input')) as f:
     puzzle_input = f.read().splitlines()
 
-current_step = []
-for line in puzzle_input:
-    current_step.append([])
-    for char in line:
-        current_step[-1].append(char)
+current_area = np.zeros((len(puzzle_input), len(puzzle_input[0])), dtype=int)
+fields = np.zeros((len(puzzle_input), len(puzzle_input[0])), dtype=int)
+fields[:] = 1
+trees = np.zeros((len(puzzle_input), len(puzzle_input[0])), dtype=int)
+trees[:] = 10
+yards = np.zeros((len(puzzle_input), len(puzzle_input[0])), dtype=int)
+yards[:] = 100
+conversion = {".": 1, "|": 10, "#": 100}
 
-def get_next_content(area, row, col):
-    adjacent = {'#': 0, "|": 0, '.': 0}
-    for r in range(max(0, row-1), min(len(area), row+2)):
-        for c in range(max(0, col-1), min(len(area[0]), col+2)):
-            if r == row and c == col:
-                continue
-            adjacent[area[r][c]] += 1
+for row, line in enumerate(puzzle_input):
+    for col, char in enumerate(line):
+        current_area[row][col] = conversion[char]
 
-    if area[row][col] == '.':
-        return '|' if adjacent['|'] >= 3 else '.'
-    elif area[row][col] == '|':
-        return '#' if adjacent['#'] >= 3 else '|'
-    else:
-        if adjacent['|'] > 0 and adjacent['#'] > 0:
-            return '#'
-        else:
-            return '.'
+filter = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
 
-
-next_step = copy.deepcopy(current_step)
-
-for n in range(0, 10):
-    if n % 1000 == 0:
-        print(n)
-    for r in range(len(current_step)):
-        next_step[r] = [get_next_content(current_step, r, c) for c in range(len(current_step[0]))]
-
-    current_step, next_step = next_step, current_step
-
-for ns in next_step:
-    print(''.join(ns))
-
-counts = {'#': 0, "|": 0, '.': 0}
-for row in current_step:
-    for col in row:
-        counts[col] += 1
-
-print(counts)
-
-print(counts['|']*counts['#'])
+for n in range(0, 1000):
+    sums = scipy.signal.convolve2d(current_area, filter, mode='same')
+    new_area = np.where(np.logical_and(current_area == 1, sums % 100 >= 30), trees, current_area)
+    new_area = np.where(np.logical_and(current_area == 10, sums % 1000 >= 300), yards, new_area)
+    new_area = np.where(np.logical_and(current_area == 100, np.logical_and(sums % 100 >= 10, sums % 1000 >= 100)), yards, new_area)
+    new_area = np.where(np.logical_and(current_area == 100, np.logical_not(np.logical_and(sums % 100 >= 10, sums % 1000 >= 100))), fields, new_area)
+    current_area, new_area = new_area, current_area
+    treesum = np.sum(current_area == 10)
+    yardsum = np.sum(current_area == 100)
+    print(f"{n}: {treesum*yardsum}")
